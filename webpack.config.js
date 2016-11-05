fe3d015e0e7765f8fb77fad809256a1b0ff670a0
Bryan Choe <bryan.choe88@gmail.com>
2016-11-04 18:05:58 -1000

    updates package.json
    add server script and changed start to npm run server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 'use strict';
-
 // Modules
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-
 /**
  * Env
  * Get npm lifecycle event to identify the environment
@@ -14,7 +12,6 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ENV = process.env.npm_lifecycle_event;
 var isTest = ENV === 'test' || ENV === 'test-watch';
 var isProd = ENV === 'build';
-
 module.exports = function makeWebpackConfig () {
   /**
    * Config
@@ -22,7 +19,6 @@ module.exports = function makeWebpackConfig () {
    * This is the object where all configuration gets set
    */
   var config = {};
-
   /**
    * Entry
    * Reference: http://webpack.github.io/docs/configuration.html#entry
@@ -32,7 +28,6 @@ module.exports = function makeWebpackConfig () {
   config.entry = isTest ? {} : {
     app: './src/app/app.js'
   };
-
   /**
    * Output
    * Reference: http://webpack.github.io/docs/configuration.html#output
@@ -42,20 +37,16 @@ module.exports = function makeWebpackConfig () {
   config.output = isTest ? {} : {
     // Absolute output directory
     path: __dirname + '/dist',
-
     // Output path from the view of the page
     // Uses webpack-dev-server in development
     publicPath: isProd ? '/' : 'http://localhost:8080/',
-
     // Filename for entry points
     // Only adds hash in build mode
     filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
-
     // Filename for non-entry points
     // Only adds hash in build mode
     chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
   };
-
   /**
    * Devtool
    * Reference: http://webpack.github.io/docs/configuration.html#devtool
@@ -68,14 +59,12 @@ module.exports = function makeWebpackConfig () {
   } else {
     config.devtool = 'eval-source-map';
   }
-
   /**
    * Loaders
    * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
    * List: http://webpack.github.io/docs/list-of-loaders.html
    * This handles most of the magic responsible for converting modules
    */
-
   // Initialize module
   config.module = {
     preLoaders: [],
@@ -118,7 +107,6 @@ module.exports = function makeWebpackConfig () {
       loader: 'raw'
     }]
   };
-
   // ISPARTA LOADER
   // Reference: https://github.com/ColCh/isparta-instrumenter-loader
   // Instrument JS files with Isparta for subsequent code coverage reporting
@@ -133,7 +121,6 @@ module.exports = function makeWebpackConfig () {
       loader: 'isparta-loader'
     })
   }
-
   /**
    * PostCSS
    * Reference: https://github.com/postcss/autoprefixer-core
@@ -144,14 +131,12 @@ module.exports = function makeWebpackConfig () {
       browsers: ['last 2 version']
     })
   ];
-
   /**
    * Plugins
    * Reference: http://webpack.github.io/docs/configuration.html#plugins
    * List: http://webpack.github.io/docs/list-of-plugins.html
    */
   config.plugins = [];
-
   // Skip rendering index.html in test mode
   if (!isTest) {
     // Reference: https://github.com/ampedandwired/html-webpack-plugin
@@ -161,29 +146,24 @@ module.exports = function makeWebpackConfig () {
         template: './src/public/index.html',
         inject: 'body'
       }),
-
       // Reference: https://github.com/webpack/extract-text-webpack-plugin
       // Extract css files
       // Disabled when in test mode or not in build mode
       new ExtractTextPlugin('[name].[hash].css', {disable: !isProd})
     )
   }
-
   // Add build specific plugins
   if (isProd) {
     config.plugins.push(
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
       // Only emit files when there are no errors
       new webpack.NoErrorsPlugin(),
-
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
       // Dedupe modules in the output
       new webpack.optimize.DedupePlugin(),
-
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
       new webpack.optimize.UglifyJsPlugin(),
-
       // Copy assets from the public folder
       // Reference: https://github.com/kevlened/copy-webpack-plugin
       new CopyWebpackPlugin([{
@@ -191,7 +171,6 @@ module.exports = function makeWebpackConfig () {
       }])
     )
   }
-
   /**
    * Dev server configuration
    * Reference: http://webpack.github.io/docs/configuration.html#devserver
@@ -201,6 +180,5 @@ module.exports = function makeWebpackConfig () {
     contentBase: './src/public',
     stats: 'minimal'
   };
-
   return config;
 }();
