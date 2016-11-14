520f2d620c67e2eb1ec8424d4af33849da8ca195
Casey Oeda <coeda@live.com>
2016-11-14 09:25:47 -1000

    update

--- src/app/services/photos.js
+++ src/app/services/photos.js
@@ -8,8 +8,6 @@ export const PhotosService = [
     }
 
     getPhotos(id){
-      console.log("hit getphotos")
-      console.log("id",id)
       return this.$http.jsonp(`https://api.instagram.com/v1/users/${id}/media/recent/?&callback=JSON_CALLBACK&access_token=175690487.02eff85.fd0b74d4431044a9b82fc9a925d036ad`);
     }
 
