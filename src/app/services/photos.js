402c3b9f62578650db583df3e247938ed5e5f32b
Casey Oeda <coeda@live.com>
2016-11-17 19:23:20 -1000

    added buttons to sorted locations

--- src/app/services/photos.js
+++ src/app/services/photos.js
@@ -15,9 +15,6 @@ export const PhotosService = [
     return this.$http.jsonp("https://api.instagram.com/v1/users/self/followed-by?count=99&&callback=JSON_CALLBACK&access_token=175690487.5dae370.360702f1fdf248ae87636bb61663f602");
     }
 
-    getLocation(){
-    return this.$http.jsonp("https://api.instagram.com/v1/media/search?lat=21.2922381&lng=-157.8237538&distance=5000&callback=JSON_CALLBACK&access_token=175690487.02eff85.fd0b74d4431044a9b82fc9a925d036ad");
-    }
   }
 ];
 
