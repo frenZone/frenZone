509606f9ecf13757f3db23c952aa6dab5fec01f8
Casey Oeda <coeda@live.com>
2016-11-23 15:04:47 -1000

    added server.js, moved index.html out to index.tpl.html and creates a index.html file from webpack

---
+++ server.js
@@ -0,0 +1,65 @@
+//Essentials
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+
+app.use(express.static('./src/public'));
+app.use(bp.urlencoded({extended : true}));
+
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 8080 : process.env.PORT;
+
+if (isDeveloping) {
+  app.set('host', 'http://localhost');
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+    },
+  });
+  const response = (req, res) => {
+    res.write(middleware.fileSystem.readFileSync(path.resolve(__dirname, 'dist/index.html')));
+    res.end();
+  };
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', response);
+} else {
+  app.use(express.static(`${__dirname}/dist`));
+  app.get('*', (req, res) => {
+    res.write(
+      fs.readFileSync(path.resolve(__dirname, 'dist/index.html'))
+    );
+  });
+}
+
+
+
+const onStart = (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.info(
+    `==> 🌎 Listening on port ${port}. ` +
+    `Open up http://localhost:${port}/ in your browser.`
+  );
+};
+
+app.listen(port, 'localhost', onStart);
+
+module.exports = app;
